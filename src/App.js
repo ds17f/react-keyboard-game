66e664a781d48fa2257c7661aa24255e329777d2
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-07-06 12:01:31 -0700

    add a color game

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import './App.css';
 
 import { KeyResponse } from './components/KeyResponse';
 import { Prompt } from './components/Prompt';
-import { GameBoard } from "./components/Game/LetterSearch";
+// import { GameBoard } from "./components/Game/LetterSearch";
+import { GameBoard } from "./components/Game/ColorMatch/gameboard"
 
 function App() {
   const [goal, setGoal] = useState(null);
   const [guess, setGuess] = useState(null);
-  const [isBeta, setBeta] = useState(false);
+  const [isBeta, setBeta] = useState(true);
 
   return (
     <div className="App">
       <Prompt setGoal={setGoal}/>
       { isBeta
-        ? <GameBoard/>
+        ? <GameBoard columns={3} rows={3}/>
         : <KeyResponse audioPath="set1/" setGuess={setGuess} />
       }
       {
---
+++ src/components/Game/ColorMatch/gameboard.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {Table} from "react-bootstrap";
+import speechSynth from "speech-synthesis";
+import { lightOrDark } from "../../../lib";
+
+const shuffleArray = (array) => {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+
+export const GameBoard = ({rows, columns}) => {
+  const colorList = [
+    "red","blue","green",
+    "pink","purple","yellow",
+    "white","black", "orange"
+  ];
+
+  const makeMatrix = (x, y, source) => {
+    let index = 0;
+    const matrix = [];
+    for (let row = 0; row < y; row++) {
+      matrix.push([]);
+      for (let col = 0; col < x; col++) {
+        matrix[row].push(source[index]);
+        index++;
+      }
+    }
+    return matrix;
+  };
+
+
+
+
+  const payload = (data) => {
+    // return <SelectCell style={cellStyle} content={col} selectedColor={colorMap[col]} audio={audio}/>
+  };
+
+  // return (
+  //   <div>{JSON.stringify(makeMatrix(3,3,shuffledColors))}</div>
+  // );
+
+  const rowHeight = (window.innerHeight - 30) / rows;
+  const colWidth = (window.innerWidth * .8) / columns;
+
+  const cellStyle = {
+    display: "inline-block",
+    width: colWidth,
+    height: rowHeight,
+    verticalAlign: "middle",
+    textAlign: "center",
+    fontSize: 32
+  };
+
+  const matrix = makeMatrix(rows, columns, shuffleArray(colorList));
+  console.log(matrix)
+  return (
+    <Table className="ColorMatch" onClick={(e) => {e.preventDefault()}}>
+      <tbody onClick={(e) => {e.preventDefault()}}>
+      {
+        matrix.map(row => {
+          return <tr onClick={(e) => {e.preventDefault()}}>
+            {
+              row.map(col => {
+                const style = Object.assign({}, cellStyle, {background: col});
+                style.color = lightOrDark(col) === "light" || col === "black" ? "white" : "black";
+                return <td onClick={() => speechSynth(col, 'en-US')} style={style}>{col[0].toUpperCase()}</td>
+              })
+            }
+          </tr>
+        })
+
+      }
+      </tbody>
+    </Table>
+  )
+
+};
